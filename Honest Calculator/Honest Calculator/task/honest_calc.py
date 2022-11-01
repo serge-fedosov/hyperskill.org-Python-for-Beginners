b6cd12b0b8dcf6f580b9ad825cd09883670a2c7c
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-11-01 12:49:02 +0400

    Honest Calculator - Stage 4/5

--- Honest Calculator/Honest Calculator/task/honest_calc.py
+++ Honest Calculator/Honest Calculator/task/honest_calc.py
@@ -6,10 +6,35 @@ msg_2 = "Yes ... an interesting math operation. You've slept through all classes
 msg_3 = "Yeah... division by zero. Smart move..."
 msg_4 = "Do you want to store the result? (y / n):"
 msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = "You are"
+
+
+def check(v1, v2, v3):
+    msg = ""
+
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg += msg_6
+
+    if (v1 == 1 or v2 == 1) and v3 == "*":
+        msg += msg_7
+
+    if (v1 == 0 or v2 == 0) and (v3 == "*" or v3 == "+" or v3 == "-"):
+        msg += msg_8
+
+    if msg != "":
+        msg = msg_9 + msg
+        print(msg)
+
+
+def is_one_digit(v):
+    return -10 < v < 10 and v.is_integer()
+
 
 memory = 0
 repeat = True
-
 while repeat:
     print(msg_0)
     calc = input()
@@ -31,6 +56,8 @@ while repeat:
     if oper not in operations:
         print(msg_2)
         continue
+
+    check(x, y, oper)
 
     if oper == "/" and y == 0:
         print(msg_3)
@@ -70,17 +97,3 @@ while repeat:
                 continue
 
             repeat = False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
