746476cf8a7cc895bb81eeccbbc0ca81b87bf905
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-11-02 15:49:33 +0400

    Last Pencil - Stage 3/5

--- Last Pencil/Last Pencil/task/game.py
+++ Last Pencil/Last Pencil/task/game.py
@@ -1,8 +1,15 @@
-print("How many pencils would you like to use:")
-n = int(input())
+names = ["John", "Jack"]
 
-print("Who will be the first (John, Jack):")
+print("How many pencils would you like to use:")
+pencils = int(input())
+
+print(f"Who will be the first ({names[0]}, {names[1]}):")
 name = input()
 
-print("|" * n)
-print(f"{name} is going first!")
+player = 0 if name == names[0] else 1
+while pencils > 0:
+    print("|" * pencils)
+    print(f"{names[player]}'s turn:")
+    take = int(input())
+    pencils -= take
+    player = 1 if player == 0 else 0
--- Last Pencil/Last Pencil/task/tests.py
+++ Last Pencil/Last Pencil/task/tests.py
@@ -3,7 +3,6 @@ import re
 
 
 class LastPencilTest(StageTest):
-
     @dynamic_test()
     def CheckOutput(self):
         main = TestedProgram()
@@ -26,16 +25,18 @@ class LastPencilTest(StageTest):
         return CheckResult.correct()
 
     test_data = [
-        [5, 1],
-        [20, 1],
-        [300, 1],
-        [5, 2],
-        [20, 2],
-        [300, 2]
+        [5, 1, [2, 1, 2]],
+        [20, 1, [3, 2, 3, 1, 2, 3, 3, 3]],
+        [30, 1, [3, 2, 3, 1, 2, 3, 3, 3, 2, 1, 2, 3, 2]],
+        [15, 1, [8, 9]],
+        [5, 2, [2, 1, 2]],
+        [20, 2, [3, 2, 3, 1, 2, 3, 3, 3]],
+        [30, 2, [3, 2, 3, 1, 2, 3, 3, 3, 2, 1, 2, 3, 2]],
+        [15, 2, [8, 9]]
     ]
 
     @dynamic_test(data=test_data)
-    def CheckGame(self, amount, first):
+    def CheckGame(self, amount, first, moves):
         main = TestedProgram()
         main.start()
         output2 = main.execute(str(amount))
@@ -44,13 +45,17 @@ class LastPencilTest(StageTest):
         leftName = output2[output2.rfind('(') + 1:output2.rfind(',')]
         rightName = output2[output2.rfind(',') + 1:output2.rfind(')')]
 
-        firstName = ""
+        prevPlayer = ""
+        nextPlayer = ""
         if first == 1:
-            firstName = leftName
+            prevPlayer = leftName
+            nextPlayer = rightName
         else:
-            firstName = rightName
+            prevPlayer = rightName
+            nextPlayer = leftName
 
-        output3 = main.execute(firstName).lower()
+        output3 = main.execute(prevPlayer).lower()
+
         lines = output3.strip().split('\n')
         linesNonEmpty = [s for s in lines if len(s) != 0]
 
@@ -58,7 +63,7 @@ class LastPencilTest(StageTest):
             raise WrongAnswer("When the player provides the initial game conditions"
                               ", your program should print 2 non-empty lines:\n"
                               "one with with vertical bar symbols representing the number of pencils, "
-                              "the other with the \"*NameX* is going first\" string.\n"
+                              "the other with the \"*NameX* turn\" string.\n"
                               f"{len(linesNonEmpty)} lines were found in the output.")
 
         checkPencils = [s for s in lines if '|' in s]
@@ -73,16 +78,59 @@ class LastPencilTest(StageTest):
         if len(list(set(checkPencils[0]))) != 1:
             raise WrongAnswer("The line with pencils should not contain any symbols other than the '|' symbol.")
 
-        if len(checkPencils[0]) != amount:
+        if len(checkPencils[0]) != int(amount):
             raise WrongAnswer("The line with pencils should contain as many '|' symbols as the player provided.")
 
-        checkTurn = any((firstName.lower() in s) and ("first" in s) for s in lines)
+        checkTurn = any((prevPlayer.lower() in s) and ("turn" in s) for s in lines)
 
         if not checkTurn:
-            raise WrongAnswer(f"There should be a line in the output that contains the \"{firstName} is going first!\""
-                              f" string if {firstName} is the first player.")
+            raise WrongAnswer(f"When the player provides the initial game conditions"
+                              f" there should be a line in output that contains the \"{prevPlayer}\'s turn\""
+                              f" string if {prevPlayer} is the first player.")
+
+        onTable = amount
+
+        for i in moves:
+            onTable -= i
+            output = main.execute(str(i)).lower()
+            lines = output.strip().split('\n')
+            linesNonEmpty = [s for s in lines if len(s) != 0]
+
+            if onTable <= 0:
+                if len(linesNonEmpty) != 0:
+                    raise WrongAnswer("After the last pencil is taken, there should be no output.")
+                else:
+                    break
+
+            if len(linesNonEmpty) != 2:
+                raise WrongAnswer("When one of the players enters the amount of pencils they want to remove"
+                                  ", your program should print 2 non-empty lines.")
+
+            checkPencils = [s for s in lines if '|' in s]
+            if len(checkPencils) == 0:
+                raise WrongAnswer("When one of the players enters the amount of pencils they want to remove"
+                                  ", your program should print one line with vertical bar symbols ('|') representing "
+                                  "pencils.")
+            if len(checkPencils) > 1:
+                raise WrongAnswer("When one of the players enters the amount of pencils they want to remove"
+                                  ", your program should print only one line with vertical bar symbols ('|') "
+                                  "representing pencils.")
+            if len(list(set(checkPencils[0]))) != 1:
+                raise WrongAnswer("The line with pencils should not contain any symbols other than the '|' symbol.")
+
+            if len(checkPencils[0]) != onTable:
+                raise WrongAnswer("When one of the players enters the amount of pencils they want to remove, "
+                                  "the line with pencils should contain as many '|' symbols as there are pencils left.")
+
+            checkTurn = any((nextPlayer.lower() in s) and ("turn" in s) for s in lines)
+
+            if not checkTurn:
+                raise WrongAnswer(f"When {prevPlayer} enters the amount of pencils they want to remove, "
+                                  f"there should be a line in the output that contains \"{nextPlayer} turn\".")
+            prevPlayer, nextPlayer = nextPlayer, prevPlayer
         if not main.is_finished():
-            raise WrongAnswer("Your program should not request anything after initial conditions have been printed.")
+            raise WrongAnswer("Your program should not request anything when there are no pencils left.")
+
         return CheckResult.correct()
 
 
